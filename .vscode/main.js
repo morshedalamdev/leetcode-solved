d4091a01230ef857a742680f6d9c87f8b3b89b2a
Morshed Alam <65453251+morshedalamdev@users.noreply.github.com>
2026-05-16 02:39:47 +0000

    Fix formatting issue in function composition implementation

--- .vscode/main.js
+++ .vscode/main.js
@@ -15,7 +15,7 @@ var compose = function(functions) {
     
     return function(x) {
         for(let i = functions.length - 1; i >= 0; i--){
-            const fn = functions[i];˝
+            const fn = functions[i];
             x = fn(x)
         }
         console.log(x)
